13f52989c013f9cb23102685431776b198fb2eea
jungnaldo <jjs921203@gmail.com>
2022-09-02 15:22:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import Box from "./component/Box";
 
 function App() {
+  const count = useSelector((state) => state.count);
+  const id = useSelector((state) => state.id);
+  const password = useSelector((state) => state.password);
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({ type: "INCREMENT", payload: { num: 5 } });
+  };
+  const decrease = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  const Login = () => {
+    dispatch({ type: "LOGIN", payload: { id: "noona", password: "123" } });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {id},{password}
+      </h1>
+      <h1>{count}</h1>
+      <button onClick={increase}>증가</button>
+      <button onClick={Login}>login</button>
+      <button onClick={decrease}>감소</button>
+      <Box />
     </div>
   );
 }
---
+++ src/redux/reducer/reducer.js
@@ -0,0 +1,24 @@
+let initialState = {
+  count: 0,
+  id: "",
+  password: "",
+};
+function reducer(state = initialState, action) {
+  console.log("action은 뭘까", action);
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + action.payload.num };
+    case "LOGIN":
+      return {
+        ...state,
+        id: action.payload.id,
+        password: action.payload.password,
+      };
+    case "DECREMENT":
+      return { ...state, count: state.count - 1 };
+    default:
+      return { ...state };
+  }
+}
+
+export default reducer;
